0610e564e1df53fd437e82fb2f7073a1776bd61b
Christian Starek <christian.starek@technikum-wien.at>
2012-11-04 12:54:27 +0100

    first created. bloodpressure as template

---
+++ AHD/src/com/example/XML/SOAP.java
@@ -0,0 +1,52 @@
+package com.example.XML;
+
+import java.io.IOException;
+
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPBodyElement;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPHeaderElement;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.SOAPPart;
+import javax.xml.transform.Source;
+
+public class SOAP {
+
+	public String header = "";
+	public String body = "";
+	
+	public SOAP (String hl7msg) {
+		
+		try {
+			SOAPMessage sm = MessageFactory.newInstance().createMessage();
+			SOAPPart sp = sm.getSOAPPart();
+			SOAPEnvelope se = sp.getEnvelope();
+			
+			SOAPHeader sh = se.getHeader();
+			sh.addNamespaceDeclaration("wsa", "http://www.w3.org/2005/08/addressing");
+			SOAPHeaderElement she_to = sh.addHeaderElement(se.createName("To", "wsa", "http://localhost/DeviceObservationConsumer_Service"));
+			she_to.addAttribute(se.createName("mustUnderstand"),"true");
+			
+			SOAPBody sb = se.getBody();
+			SOAPBodyElement sbe = sb.addBodyElement(se.createName("CommunicatePCDData", "xxx", hl7msg));
+			sbe.addAttribute(se.createName("xmlns"), "urn:ihe:pcd:dec:2010");
+			
+			Source source = sp.getContent();
+			
+			sm.writeTo(System.out);
+			System.out.println();
+			
+		} catch (SOAPException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}//SOAP
+	
+}
---
+++ AHD/src/com/example/XML/SOAPhard.java
@@ -0,0 +1,26 @@
+package com.example.XML;
+
+public class SOAPhard {
+
+	public String header = "";
+	public String body = "";
+	private String all = "";
+	
+	public SOAPhard (String hl7msg) {
+	
+		all = "<soapenv:Envelope xmlns:soapenv=\"http://www.w3.org/2003/05/soap-envelope\">";
+		header = "soapenv:Header xmlns:wsa=\"http://www.w3.org/2005/08/addressing\"><wsa:To soapenv:mustUnderstand=\"true\">http://localhost/DeviceObservationConsumer_Service</wsa:To>" +
+				"<wsa:From soapenv:mustUnderstand=\"true\"><wsa:Address>http://www.w3.org/2005/08/addressing/anonymous</wsa:Address></wsa:From><wsa:MessageID soapenv:mustUnderstand=\"true\">urn:uuid:A52590343911955D1A1251497585530</wsa:MessageID>" +
+				"<wsa:Action soapenv:mustUnderstand=\"true\">urn:ihe:pcd:2010:CommunicatePCDData</wsa:Action> </soapenv:Header>";
+		body = "<soapenv:Body><CommunicatePCDData xmlns=\"urn:ihe:pcd:dec:2010\">"+hl7msg+"</CommunicatePCDData></soapenv:Body>";
+		all += header;
+		all += body;
+		all += "</soapenv:Envelope>";
+	}//SOAP
+	
+	public String getSoap () {
+		return all;
+	}//getSoap
+	
+	
+}
